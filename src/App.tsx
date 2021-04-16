3e67fb02fec1f212decab0685a319c199513fec1
cbravos <cnbravos5@gmail.com>
2021-04-16 15:33:46 -0300

    build: add section and button style

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Button from "./components/Button";
+import Todo from "./components/Todo";
+import TodoSection from "./components/TodoSection";
+import "./styles/page.css";
+import "./styles/todo.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="section">
+        <p>Click to add a new ToDo</p>
+        <Button>new</Button>
+      </div>
+      <div className="dnd-container">
+        <TodoSection secName="Pending" color="warning">
+          <Todo title="Make a Todo" />
+        </TodoSection>
+
+        <TodoSection secName="Completed" color="success"></TodoSection>
+      </div>
+    </>
   );
 }
 
