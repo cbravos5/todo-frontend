51d0b588ac2f81234b29225bc9bd140096ed2866
cbravos <cnbravos5@gmail.com>
2021-04-16 22:06:56 -0300

    build: add beautiful-dnd package and useful data

---
+++ src/data.ts
@@ -0,0 +1,17 @@
+const data: any = {
+  todos: {
+    "todo-1": { id: "todo-1", content: "Teste 1" },
+    "todo-2": { id: "todo-2", content: "Teste 2" },
+    "todo-3": { id: "todo-3", content: "Teste 3" },
+    "todo-4": { id: "todo-4", content: "Teste 4" },
+  },
+  columns: {
+    "column-1": {
+      id: "column-1",
+      title: "To do",
+      todoIds: ["todo-1", "todo-2", "todo-3", "todo-4"],
+    },
+  },
+};
+
+export default data;
