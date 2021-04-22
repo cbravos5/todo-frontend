b3cbb0e2bb252d1bd7ce502b30a3b4b8f779df82
cbravos <cnbravos5@gmail.com>
2021-04-22 11:20:45 -0300

    add 'new todo' action

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -4,15 +4,29 @@ import DatePicker from "react-datepicker";
 import { useGlobalContext } from "../context";
 
 const TodoForm: React.FC<Record<string, never>> = () => {
-  const { setShowForm } = useGlobalContext();
-
-  const [startDate, setStartDate] = useState<Date | [Date, Date] | null>(
-    new Date(),
-  );
+  const { state, setState, setShowForm } = useGlobalContext();
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [deadline, setDeadline] = useState<Date | [Date, Date] | null>(null);
 
   const handleInput = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log("Sent");
+    const newTodo = { id: title, content: title };
+
+    const hasTodo = state.todos[newTodo.id];
+
+    if (hasTodo) {
+      console.log("A Todo with this title already exists");
+      return;
+    }
+
+    const newState = { ...state };
+
+    newState.todos[newTodo.id] = newTodo;
+    newState.columns.Pending.todoIds.push(newTodo.id);
+
+    setState(newState);
+    setShowForm(false);
   };
 
   return (
@@ -23,17 +37,28 @@ const TodoForm: React.FC<Record<string, never>> = () => {
       <form onSubmit={handleInput} className="form">
         <div>
           <label htmlFor="title">Title</label>
-          <input type="text" required maxLength={35} />
+          <input
+            type="text"
+            required
+            maxLength={35}
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </div>
         <div>
           <label htmlFor="description">Description</label>
-          <textarea maxLength={120} rows={3} />
+          <textarea
+            maxLength={120}
+            rows={3}
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
         </div>
         <div>
           <label htmlFor="deadline">Deadline</label>
           <DatePicker
-            selected={startDate as Date}
-            onChange={(date) => setStartDate(date)}
+            selected={deadline as Date}
+            onChange={(date) => setDeadline(date)}
           />
         </div>
         <button type="submit" className="btn btn-success">
